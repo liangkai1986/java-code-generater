35425e84e53dbffd953a803df5666ba8dcc869e5
liangkai1986 <liangkai19860118@163.com>
2019-05-20 17:01:44 +0800

    增加获取java包装类的方法

--- src/main/java/com/smartwater/generator/utils/TypeUtil.java
+++ src/main/java/com/smartwater/generator/utils/TypeUtil.java
@@ -8,7 +8,7 @@ import java.sql.Types;
 public class TypeUtil {
 
     /**
-     * 将数据库数据类型转换为Java数据类型
+     * 将数据库数据类型转换为Java基本数据类型
      *
      * @param sqlType
      * @return
@@ -38,6 +38,80 @@ public class TypeUtil {
             case Types.FLOAT:
             case Types.DOUBLE:
                 sb.append("double");
+                break;
+            case Types.DECIMAL:
+            case Types.NUMERIC:
+                sb.append("BigDecimal");
+                break;
+            case Types.VARCHAR:
+            case Types.CHAR:
+            case Types.NCHAR:
+            case Types.NVARCHAR:
+            case Types.LONGVARCHAR:
+            case Types.LONGNVARCHAR:
+                sb.append("String");
+                break;
+            case Types.DATE:
+                sb.append("Date");
+                break;
+            case Types.TIME:
+                sb.append("Time");
+                break;
+            case Types.TIMESTAMP:
+                sb.append("Timestamp");
+                break;
+            case Types.NCLOB:
+            case Types.CLOB:
+            case Types.BLOB:
+            case Types.BINARY:
+            case Types.VARBINARY:
+            case Types.LONGVARBINARY:
+                sb.append("byte[]");
+                break;
+            case Types.NULL:
+            case Types.OTHER:
+            case Types.JAVA_OBJECT:
+                sb.append("Object");
+                break;
+            default:
+                sb.append("Object");
+
+        }
+        return sb.toString();
+    }
+
+
+    /**
+     * 将数据库数据类型转换为Java包装类数据类型
+     *
+     * @param sqlType
+     * @return
+     */
+    public static String parseWrapperClassFormSqlType(int sqlType) {
+        StringBuilder sb = new StringBuilder();
+        switch (sqlType) {
+            case Types.BIT:
+            case Types.BOOLEAN:
+                sb.append("Boolean");
+                break;
+            case Types.TINYINT:
+                sb.append("Byte");
+                break;
+            case Types.SMALLINT:
+                sb.append("Short");
+                break;
+            case Types.INTEGER:
+                sb.append("Integer");
+                break;
+            case Types.BIGINT:
+                sb.append("Long");
+                break;
+            case Types.REAL:
+                sb.append("Float");
+                break;
+            case Types.FLOAT:
+            case Types.DOUBLE:
+                sb.append("Double");
                 break;
             case Types.DECIMAL:
             case Types.NUMERIC:
